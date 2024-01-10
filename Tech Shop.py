f359eeb1fd1e31d9df088b809e469a98d43bf8b5
Θεόδωρος Ανανιάδης <95680864+ThodwrisAnaniadis@users.noreply.github.com>
2024-01-10 17:47:41 +0200

    Add files via upload

---
+++ Tech Shop.py
@@ -0,0 +1,22 @@
+class Item:
+    pay_rate = 0.8 # The pay rate after 20% discount
+    def __init__(self, name: str, price: float, quantity=0):
+        
+       # Run validations to the received arguments
+        assert price >= 0, f"Price {price} is not greater then zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater then zero!"
+       
+        # Assign to self object
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        
+    def calculate_total_price(self):
+        return self.price * self.quantity
+
+item1 = Item("Phone",100,5)
+
+item2 = Item("Laptop",1000,3)
+
+print(item1.calculate_total_price())
+print(item2.calculate_total_price())
